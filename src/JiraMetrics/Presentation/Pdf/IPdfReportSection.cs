1cc9eeb2a5d4f3b882613509d2ee605ce1d74ebd
Nikita Tulenev <n1tulenev@gmail.com>
2026-03-28 08:46:42 +0100

    Add xmldoc

--- src/JiraMetrics/Presentation/Pdf/IPdfReportSection.cs
+++ src/JiraMetrics/Presentation/Pdf/IPdfReportSection.cs
@@ -4,7 +4,15 @@ using QuestPDF.Fluent;
 
 namespace JiraMetrics.Presentation.Pdf;
 
+/// <summary>
+/// Represents one composable section of the generated PDF report.
+/// </summary>
 internal interface IPdfReportSection
 {
+    /// <summary>
+    /// Composes the section content into the target PDF column.
+    /// </summary>
+    /// <param name="column">Target QuestPDF column descriptor.</param>
+    /// <param name="reportData">Aggregated report data.</param>
     void Compose(ColumnDescriptor column, JiraPdfReportData reportData);
 }
