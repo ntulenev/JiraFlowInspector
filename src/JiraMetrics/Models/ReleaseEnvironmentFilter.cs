c76ce9ca4d5255256dfa4a8d50c8eb8daba1570a
Nikita Tulenev <n1tulenev@gmail.com>
2026-07-11 07:12:56 +0200

    Complete ReleaseEnvironmentFilter XML documentation

--- src/JiraMetrics/Models/ReleaseEnvironmentFilter.cs
+++ src/JiraMetrics/Models/ReleaseEnvironmentFilter.cs
@@ -5,4 +5,6 @@ namespace JiraMetrics.Models;
 /// <summary>
 /// Represents an environment filter for release issue reads.
 /// </summary>
+/// <param name="FieldName">The <paramref name="FieldName"/> value.</param>
+/// <param name="Value">The <paramref name="Value"/> value.</param>
 public sealed record ReleaseEnvironmentFilter(JiraFieldName FieldName, JiraFieldValue Value);
