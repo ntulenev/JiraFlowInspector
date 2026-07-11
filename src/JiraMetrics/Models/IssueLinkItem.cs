aed271f618f59d593faceeb483075f4b9b6fe590
Nikita Tulenev <n1tulenev@gmail.com>
2026-07-11 07:08:27 +0200

    Document issue link record parameters

--- src/JiraMetrics/Models/IssueLinkItem.cs
+++ src/JiraMetrics/Models/IssueLinkItem.cs
@@ -5,4 +5,6 @@ namespace JiraMetrics.Models;
 /// <summary>
 /// Lightweight issue-link row.
 /// </summary>
+/// <param name="Key">Linked issue key.</param>
+/// <param name="RelationName">Jira link relation name.</param>
 public sealed record IssueLinkItem(IssueKey Key, string RelationName);
