a28cb79655e6c8ba242de5c14306ef6b76baac2d
Nikita Tulenev <n1tulenev@gmail.com>
2026-07-11 07:14:18 +0200

    Complete IReleaseIssuesJqlBuilder XML documentation

--- src/JiraMetrics/Abstractions/Api/Jql/IReleaseIssuesJqlBuilder.cs
+++ src/JiraMetrics/Abstractions/Api/Jql/IReleaseIssuesJqlBuilder.cs
@@ -11,6 +11,8 @@ public interface IReleaseIssuesJqlBuilder
     /// <summary>
     /// Builds the release issue search query.
     /// </summary>
+    /// <param name="request">Release issue read criteria.</param>
+    /// <returns>The release issue JQL query.</returns>
     JqlQuery BuildQuery(ReleaseIssueReadRequest request);
 }
 
